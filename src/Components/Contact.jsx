3b4d45fa94d6b6080f1dadad2f081d085ee10942
ibukunfolay <70657237+ibukunfolay@users.noreply.github.com>
2022-06-11 14:57:29 +0100

    update form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: "auto",
     bgcolor: "background.paper",
     boxShadow: 24,
-    p: 4,
+    p: 2,
   };
 
   return (
@@ -44,35 +44,18 @@ const Contact = () => {
         <Box sx={style}>
           <form class="w-full max-w-lg">
             <div class="flex flex-wrap -mx-3 mb-6">
-              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="grid-first-name"
-                >
-                  First Name
-                </label>
-                <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="Jane"
-                />
-                <p class="text-red-500 text-xs italic">
-                  Please fill out this field.
-                </p>
-              </div>
-              <div class="w-full md:w-1/2 px-3">
+              <div class="w-full px-3">
                 <label
                   class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   for="grid-last-name"
                 >
-                  Last Name
+                  Full Name
                 </label>
                 <input
                   class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   id="grid-last-name"
                   type="text"
-                  placeholder="Doe"
+                  placeholder="Full Name"
                 />
               </div>
             </div>
@@ -106,10 +89,6 @@ const Contact = () => {
                   class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
                   id="message"
                 ></textarea>
-                <p class="text-gray-600 text-xs italic">
-                  Re-size can be disabled by set by resize-none / resize-y /
-                  resize-x / resize
-                </p>
               </div>
             </div>
             <div class="md:flex md:items-center">
